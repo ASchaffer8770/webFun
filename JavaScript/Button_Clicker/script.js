25b93c37676c787276fddf6b58fb9bc341411a66
Alex Schaffer <aschaffer8770@gmail.com>
2022-07-25 11:54:15 -0700

    final html and js

---
+++ JavaScript/Button_Clicker/script.js
@@ -0,0 +1,11 @@
+function logOut(Element) {
+    Element.innerText = 'Logout';
+}
+
+function removeDef(Element) {
+    Element.remove();
+}
+
+function alertLike(Element) {
+    alert('ninja was liked')
+}
